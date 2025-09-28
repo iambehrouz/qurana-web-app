f707459bd29446c7f9c7dd15b9bcca5753bfbef3
Behrouz Zarei <6842068+iambehrouz@users.noreply.github.com>
2025-09-28 20:00:16 +0330

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,27 +2,27 @@
 # version: 1.1
 
 echo 'Pulling last verions.';
-cd /repositories/trendytrades-web-app/;
+cd /repositories/qurana-web-app/;
 git pull;
 
 echo 'Building the project.';
-cp /repositories/trendytrades-web-app/config.server.json /repositories/trendytrades-web-app/config.json;
-cd /repositories/trendytrades-web-app/;
+cp /repositories/qurana-web-app/config.server.json /repositories/qurana-web-app/config.json;
+cd /repositories/qurana-web-app/;
 rm -rf node_modules;
 source ~/.nvm/nvm.sh;
 npm i;
 npm run build;
 
 echo 'Removing old version from workspace.';
-rm -rf /workspace/trendytrades/web-app/*;
+rm -rf /workspace/qurana/web-app/*;
 
 echo 'Copying build version to workspace.';
-cp /repositories/trendytrades-web-app/* /workspace/trendytrades/web-app/ -r;
+cp /repositories/qurana-web-app/* /workspace/qurana/web-app/ -r;
 
 echo 'Restoring repository.';
 git restore .;
 
 echo 'Restarting application.';
-pm2 restart trendytrades.io;
+pm2 restart qurana.chateq.fr;
 
 echo 'Deployment has been done.';
